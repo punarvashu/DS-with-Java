e0e60177c3855e4ce1c5e7e0c0447c594a005287
punarvashu <62372494+punarvashu@users.noreply.github.com>
2020-04-17 22:36:43 +0530

    Added insertAt() , removeAt() and search() methods

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -9,6 +9,7 @@ public class SinglyLinkedList {
 		this.last = null;
 
 	}
+
 	private Node _createNode(int data) {
 		return new Node(data);
 	}
@@ -46,48 +47,82 @@ public class SinglyLinkedList {
 		}
 	}
 
-public void display() {
-	if (this.first == null) {
-		System.out.println("No node is available.");
-		return;
-	} else {
-		Node currentNode = first;
-		while (currentNode != null) {
-			System.out.print(currentNode.data + " ");
-			currentNode = currentNode.next;
-		}
-	}
-}
-public void insertAtStart(int data)
-{
-	Node node = new Node();
-	node.data = data;
-	node.next = null;
-	node.next = first;
-	first = node;
-	System.out.println(" " + node.data);
-}
-public void insertAt(int index,int data)
-{
-	Node node = new Node();
-	node.data = data;
-	node.next = null;
-	
-	if(index==0)
-	{
+	public void display() {
 		if (this.first == null) {
 			System.out.println("No node is available.");
 			return;
+		} else {
+			Node currentNode = first;
+			while (currentNode != null) {
+				System.out.print(currentNode.data + " ");
+				currentNode = currentNode.next;
+			}
+		}
 	}
-	else{
-	Node n = first;
-	for(int i=0;i<index-1;i++)
-	{
-		n = n.next;
+
+	public void insertAtStart(int data) {
+		Node node = new Node();
+		node.data = data;
+		node.next = null;
+		node.next = first;
+		first = node;
 	}
-	node.next = n.next;
-	n.next = node;
+
+	public void insertAt(int index, int data) {
+		Node node = new Node();
+		node.data = data;
+		node.next = null;
+
+		if (index == 0) {
+
+			insertAtStart(data);
+		} else {
+			Node n = first;
+			for (int i = 0; i < index - 1; i++) {
+				n = n.next;
+			}
+			node.next = n.next;
+			n.next = node;
+		}
 	}
-}
-}
+
+	public void removeAt(int index) {
+		if (this.first == null) {
+			System.out.println("No node is available.");
+			System.exit(1);
+		} else {
+			Node n = first;
+			Node n1 = null;
+			for (int i = 0; i < index - 1; i++) {
+				n = n.next;
+			}
+			n1 = n.next;
+			n.next = n1.next;
+			// System.out.println("n1 " + n1.data);
+		}
+	}
+
+	public void search(int value) {
+		if (this.first == null) {
+			System.out.println("list is empty");
+			System.exit(1);
+		} else {
+			boolean flag = false;
+			Node currentNode = first;
+			while (currentNode != null) {
+				if (currentNode.data == value) {
+					flag = true;
+					break;
+				}
+				currentNode = currentNode.next;
+			}
+			if (!flag) {
+				System.out.println("Value not found.");
+			} else {
+				System.out.println("Value is present");
+			}
+		}
+
+	}
+
 }
